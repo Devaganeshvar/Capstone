aa8796dfbdc229cc63621ddad72072ae978bc5c6
Devaganeshvar <153277627+Devaganeshvar@users.noreply.github.com>
2026-08-21 10:24:00 +0530

    Update Program.cs

--- Application.Api/Program.cs
+++ Application.Api/Program.cs
@@ -176,7 +176,7 @@ using (var scope = app.Services.CreateScope())
     app.UseSwagger();
     app.UseSwaggerUI();
     
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseCors(AngularClientCors);
 
